cbf2235dc1880c0e76f1cfe34e4ceac2b2f6cbcd
Steve <stevele99@yahoo.fr>
2023-02-06 00:35:42 +0100

    Ajout de commentaires

--- assets/script.js
+++ assets/script.js
@@ -22,19 +22,13 @@ const slides = [
 
 
 
+/////////// Config des points ///////////
+//Le nombre de points s'adapte au nombre d'images dans le tableau "slides"
 
-
-var nbpoint = slides.length; //Le nombre de points s'adapte au nombre d'images dans le tableau "slides"
+var nbpoint = slides.length; 
 console.log("nombre de bullet point : "+nbpoint);
 
-
-
-
-
-
-
 var points = document.querySelector('.dots');
-
 
 for (let i=0 ; i<nbpoint; i++){		
 	//Selon le nombre de points, on cree le meme nombre de div avec la class dot puis on l'ajoute au parent qui est dots
@@ -46,44 +40,39 @@ for (let i=0 ; i<nbpoint; i++){
 
 
 
+/////////Configuration de départ///////////
 
 var caroussel= document.querySelector("#banner");
 
+var image = document.createElement("img"); //On crée une balise img
+image.classList.add("banner-img"); //On lui ajoute la class "banner-img"
+image.setAttribute("src",slides[0].image); //On lui met comme attribut src = slides[0].image, cad la propriété image du premier element du tableau slides( qui est le chemin relatif de l'image)
+caroussel.appendChild(image); //On attribut a la div #banner l'enfant image qu'on vient de créer
 
-var image = document.createElement("img");
-image.classList.add("banner-img");
-image.setAttribute("src",slides[0].image);
-caroussel.appendChild(image);
-
-
-var text = document.createElement("p");
+var text = document.createElement("p"); //Idem pour la balise p
 text.innerHTML= slides[0].tagLine;
 caroussel.appendChild(text);
 
-
-var initialdot = document.querySelector('.dots :nth-child(1)');
+var initialdot = document.querySelector('.dots :nth-child(1)'); //On active le premier dot
 initialdot.classList.add("dot_selected");
 
+var position=1; //La position va nous servir à savoir on l'on se situe dans le caroussel
 
 
 
 
 
 
-var position=1;
-
-
-
-
+///////// Les Event Listener sur les boutons //////////
 
 var flechegauche = document.querySelector('.arrow_left') ;
 
 flechegauche.addEventListener("click",function(e){
 		if(position==1){
 			console.log("Gauche");
-			position=slides.length;
+			position=slides.length; //Si on est au premier slide et qu'on fait clic vers la gauche, on arrive a la derniere slide donc la position prend pas 0 mais le nombre de slide
 			initialdot.classList.remove("dot_selected");
-			initialdot = document.querySelector('.dots :last-child');
+			initialdot = document.querySelector('.dots :last-child'); //On se déplace pas sur l'enfant précédent mais le dernier
 			initialdot.classList.add("dot_selected");
 			console.log("position = "+position);
 			image.setAttribute("src",slides[position-1].image);
@@ -91,13 +80,13 @@ flechegauche.addEventListener("click",function(e){
 
 		}else{
 			console.log("Gauche");
-			initialdot.classList.remove("dot_selected");
-			initialdot = initialdot.previousElementSibling;
-			initialdot.classList.add("dot_selected");
-			position-=1;
+			initialdot.classList.remove("dot_selected"); //On retire le dot actif
+			initialdot = initialdot.previousElementSibling; //On se place sur l'enfant précédent
+			initialdot.classList.add("dot_selected"); //On ajoute à l'enfant précédent la class dot selected pour que le point soit actif
+			position-=1; //La position diminue de 1
 			console.log("position = "+position);
-			image.setAttribute("src",slides[position-1].image);
-			text.innerHTML= slides[position-1].tagLine;
+			image.setAttribute("src",slides[position-1].image); //L'attribut src de la balise img se met a jour
+			text.innerHTML= slides[position-1].tagLine; //Idem pour le text
 		}
 	}
 );
@@ -128,43 +117,6 @@ flechedroite.addEventListener("click",function(e){
 		}
 	} 
 );
-
-
-
-
-
-
-
-/*var flechegauche = document.querySelector('.arrow_left') ;
-
-flechegauche.addEventListener("click",function(e){
-		console.log("Gauche");
-		initialdot.classList.remove("dot_selected");
-		initialdot = initialdot.previousElementSibling;
-		initialdot.classList.add("dot_selected");
-		position-=1;
-		console.log("position = "+position);
-		image.setAttribute("src",slides[position-1].image);
-		text.innerHTML= slides[position-1].tagLine;
-	}
-);
-
-
-
-var flechedroite = document.querySelector('.arrow_right') ;
-
-flechedroite.addEventListener("click",function(e){
-		console.log("Droite");
-		initialdot.classList.remove("dot_selected");
-		initialdot = initialdot.nextElementSibling;
-		initialdot.classList.add("dot_selected");
-		position+=1;
-		console.log("position = "+position);
-		image.setAttribute("src",slides[position-1].image);
-		text.innerHTML= slides[position-1].tagLine;
-	} 
-);*/
-
 
 
 
